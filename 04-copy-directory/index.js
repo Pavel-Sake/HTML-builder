dab96a75b55b5cdf4f125d304eb0eeb4ea5ca34c
Pavel-Sake <sake211091@gmail.com>
2022-11-06 17:19:06 +0300

    feat: implement 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const path = require("path");
+const fs = require("fs");
 
+const fsPromises = fs.promises
+
+const filePathToFiles = path.join(__dirname, "files");
+const filePathToFilesCopy = path.join(__dirname, "files-copy");
+
+
+
+
+// fsPromises.copyFile(filePath1, filePath2)
+
+// const projectFolder = new URL(filePath3);
+
+fsPromises.mkdir(filePathToFilesCopy, { recursive: true })
+
+
+fsPromises.readdir(filePathToFiles, {withFileTypes: true})
+    .then((results) => {
+
+        const pathFile = path.join(__dirname, "files", ``)
+
+        results.forEach((item) => {
+
+            const fileName = item.name;
+            const pathFileInput = path.join(__dirname, "files", `${fileName}`)
+            const filePathToOutput = path.join(__dirname, "files-copy", `${fileName}`);
+
+             fsPromises.copyFile(pathFileInput, filePathToOutput)
+        })
+
+    })
+    .catch( (error) => {
+        console.log(error);
+    })
